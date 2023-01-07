825e5441782b4cdf1f1880c78c2226dcf7799e6f
Jeong Dong Won <chorok446@gmail.com>
2023-01-07 20:57:48 +0900

    bug: graphql 이미지 업로드 구현 중 문제가 있어서 구현 보류중

--- src/apis/files/file.resolver.ts
+++ src/apis/files/file.resolver.ts
@@ -1,18 +1,19 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { FileService } from './file.service';
-import { FileUpload, GraphQLUpload } from 'graphql-upload';
-
-@Resolver()
-export class FileResolver {
-    constructor(
-        private readonly fileService: FileService, //
-    ) {}
-
-    @Mutation(() => [String])
-    uploadFile(
-        @Args({ name: 'files', type: () => [GraphQLUpload] })
-        files: FileUpload[],
-    ) {
-        return this.fileService.upload({ files });
-    }
-}
+// import { Args, Mutation, Resolver } from '@nestjs/graphql';
+// import { FileService } from './file.service';
+// import { FileUpload } from 'graphql-upload/Upload';
+// import GraphQLUpload from 'graphql-upload/GraphQLUpload';
+//
+// @Resolver()
+// export class FileResolver {
+//     constructor(
+//         private readonly fileService: FileService, //
+//     ) {}
+//
+//     @Mutation(() => [String])
+//     uploadFile(
+//         @Args({ name: 'files', type: () => [GraphQLUpload] })
+//         files: FileUpload[],
+//     ) {
+//         return this.fileService.upload({ files });
+//     }
+// }
--- src/apis/files/file.service.ts
+++ src/apis/files/file.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class FileService {
-    upload() {
-        // 구글 스토리지에 파일 업로드하기
-        return '~~~url';
-    }
-}
+// import { Storage } from '@google-cloud/storage';
+// import { Injectable } from '@nestjs/common';
+//
+// @Injectable()
+// export class FileService {
+//     async upload({ files }) {
+//         const waitedFiles = await Promise.all(files);
+//         console.log(waitedFiles);
+//
+//         const storage = new Storage({
+//             projectId: 'projectId',
+//             keyFilename: 'keyFilename',
+//         }).bucket('test');
+//         const results = await Promise.all(
+//             waitedFiles.map((el) => {
+//                 return new Promise((resolve, reject) => {
+//                     el.createReadStream()
+//                         .pipe(storage.file(el.filename).createWriteStream())
+//                         .on('finish', () => resolve(`images/${el.filename}`))
+//                         .on('error', () => reject());
+//                 });
+//             }),
+//         );
+//
+//         return results;
+//     }
+// }
